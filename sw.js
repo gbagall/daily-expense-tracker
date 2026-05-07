ed6a4f8a4a96257b0163695be397f96135043d60
Ganesh Bagal <66709840+gbagall@users.noreply.github.com>
2026-05-07 23:05:17 +0530

    Create sw.js
    PWA

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "expense-tracker-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/js/db.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
